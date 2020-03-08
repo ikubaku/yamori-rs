ebc648fc6d6e7cf9f621f55df8ebe948383c2e10
ikubaku <hide4d51@gmail.com>
2020-03-08 19:13:55 +0900

    [WIP] Add anything

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
+use std::collections::HashMap;
+use std::sync::RwLock;
+use config::{Config, ConfigError};
+
+lazy_static! {
+    static ref SETTINGS: RwLock<Config> = RwLock::new(Config::default());
+}
+
+fn handle_config_errors(res: Result<(), ConfigError>) {
+    if res.is_err() {
+        match res.err().unwrap() {
+            ConfigError::NotFound(_) => {
+                
+            }
+        }
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    handle_config_errors((SETTINGS.write()?.merge(config::File::with_name("/etc/yamori.conf")));
+
+    println!("{:?}", settings.try_into::<HashMap<String, String>>().unwrap());
 }
